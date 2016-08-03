d1b7cc9bdfb31beb1d2bd2b00ebd8b2b0a4d6090
Taylor Halliday <taylor.halliday@gmail.com>
2016-08-03 16:36:06 -0700

    updated project

--- redis_test.go
+++ redis_test.go
@@ -1,6 +1,8 @@
 package meshRedis
 
 import (
+	"flag"
+	"os"
 	"testing"
 	"time"
 )
@@ -14,15 +16,32 @@ func Test(t *testing.T) { TestingT(t) }
 
 type MeshRedisTest struct{}
 
+var live = flag.Bool("redis", false, "Include redis tests")
+
 var _ = Suite(&MeshRedisTest{})
 
-func (s *MeshRedisTest) SetUpTest(c *C) {
-	err := Connect(localRedisURL)
+// Active session for the tests
+var session *RedisSession
+
+func (m *MeshRedisTest) SetUpSuite(c *C) {
+	// Set the ENV
+	os.Setenv("REDIS_URL", localRedisURL)
+
+	err := SetupRedis()
 	c.Assert(err, Equals, nil)
 }
 
-func (s *MeshRedisTest) TearDownTest(c *C) {
-	err := Close()
+func (m *MeshRedisTest) TearDownSuite(c *C) {
+	err := ClosePool()
+	c.Assert(err, Equals, nil)
+}
+
+func (m *MeshRedisTest) SetUpTest(c *C) {
+	session = NewSession()
+}
+
+func (m *MeshRedisTest) TearDownTest(c *C) {
+	err := session.CloseSession()
 	c.Assert(err, Equals, nil)
 }
 
@@ -30,14 +49,9 @@ func (s *MeshRedisTest) TearDownTest(c *C) {
 // Connection
 //---------
 
-func (s *MeshRedisTest) TestCreateNewMeshRedisSession(c *C) {
+func (m *MeshRedisTest) TestCreateNewMeshRedisSession(c *C) {
 	// Test getting a new MeshRedisSession
-	session := NewSession()
 	err := session.Ping()
-	c.Assert(err, Equals, nil)
-
-	// Close the connection
-	err = session.Close()
 	c.Assert(err, Equals, nil)
 }
 
@@ -45,18 +59,16 @@ func (s *MeshRedisTest) TestCreateNewMeshRedisSession(c *C) {
 // Setting / Getting Strings
 //---------
 
-func (s *MeshRedisTest) TestSettingAString(c *C) {
+func (m *MeshRedisTest) TestSettingAString(c *C) {
 	// Test getting a new MeshRedisSession
-	session := NewSession()
 	session.SetString("foo", "bar")
 	val, err := session.GetString("foo")
 	c.Assert(val, Equals, "bar")
 	c.Assert(err, Equals, nil)
 }
 
-func (s *MeshRedisTest) TestSettingAStringWithExpiration(c *C) {
+func (m *MeshRedisTest) TestSettingAStringWithExpiration(c *C) {
 	// Test getting a new MeshRedisSession
-	session := NewSession()
 	session.SetStringWithExpiration("foo", 1, "barBaz")
 	val, err := session.GetString("foo")
 	c.Assert(val, Equals, "barBaz")
@@ -73,25 +85,23 @@ func (s *MeshRedisTest) TestSettingAStringWithExpiration(c *C) {
 // Setting / Getting Ints
 //---------
 
-func (s *MeshRedisTest) TestSettingAInteger(c *C) {
+func (m *MeshRedisTest) TestSettingAInteger(c *C) {
 	// Test getting a new MeshRedisSession
-	session := NewSession()
 	session.SetInt("foo", 101)
 	val, err := session.GetInt("foo")
 	c.Assert(val, Equals, 101)
 	c.Assert(err, Equals, nil)
 }
 
-func (s *MeshRedisTest) TestSettingAIntegerWithExpiration(c *C) {
+func (m *MeshRedisTest) TestSettingAIntegerWithExpiration(c *C) {
 	// Test getting a new MeshRedisSession
-	session := NewSession()
 	session.SetIntWithExpiration("foo", 1, 102)
 	val, err := session.GetInt("foo")
 	c.Assert(val, Equals, 102)
 	c.Assert(err, Equals, nil)
 
 	// Wait for a second and recheck the val
-	time.Sleep(1 * time.Second)
+	time.Sleep(2 * time.Second)
 	val, err = session.GetInt("foo")
 	c.Assert(val, Equals, 0)
 	c.Assert(err, Equals, nil)
@@ -101,11 +111,10 @@ func (s *MeshRedisTest) TestSettingAIntegerWithExpiration(c *C) {
 // Expire / TTL
 //---------
 
-func (s *MeshRedisTest) TestUpdatingExpirationOfAnEmptyKeyReturnsAnError(c *C) {
+func (m *MeshRedisTest) TestUpdatingExpirationOfAnEmptyKeyReturnsAnError(c *C) {
 	key := "KeyIveNeverEverUsedBefore"
 
 	// Test getting a new MeshRedisSession
-	session := NewSession()
 
 	// First Test TTL is returning the correct default
 	err := session.UpdateExpirationOfKey(key, 1)
@@ -118,16 +127,15 @@ func (s *MeshRedisTest) TestUpdatingExpirationOfAnEmptyKeyReturnsAnError(c *C) {
 	c.Assert(err, Equals, nil)
 }
 
-func (s *MeshRedisTest) TestUpdatingAKeysExpiration(c *C) {
+func (m *MeshRedisTest) TestUpdatingAKeysExpiration(c *C) {
 	key := "fooExpire"
 	value := "expireMe"
 
 	// Test getting a new MeshRedisSession
-	session := NewSession()
 	session.SetString(key, value)
 
 	// First Test TTL is returning the correct default
-	ttl, err := session.TTLForKey(key)
+	ttl, err := session.PTTLForKey(key)
 	c.Assert(err, Equals, nil)
 	c.Assert(ttl, Equals, -1)
 
@@ -141,9 +149,9 @@ func (s *MeshRedisTest) TestUpdatingAKeysExpiration(c *C) {
 	c.Assert(err, Equals, nil)
 
 	// Test Updated TTL
-	ttl, err = session.TTLForKey(key)
+	ttl, err = session.PTTLForKey(key)
 	c.Assert(err, Equals, nil)
-	c.Assert(ttl, Equals, 1)
+	c.Assert(ttl > 990, Equals, true)
 
 	// Check Value is there
 	val, err = session.GetString(key)
@@ -161,11 +169,10 @@ func (s *MeshRedisTest) TestUpdatingAKeysExpiration(c *C) {
 // Flushing All Keys
 //---------
 
-func (s *MeshRedisTest) TestFlushAllKeys(c *C) {
+func (m *MeshRedisTest) TestFlushAllKeys(c *C) {
 	key := "someKey"
 
 	// Test getting a new MeshRedisSession
-	session := NewSession()
 
 	// Set / Check initial value
 	session.SetString(key, "bar")
@@ -181,4 +188,96 @@ func (s *MeshRedisTest) TestFlushAllKeys(c *C) {
 	val, err = session.GetString(key)
 	c.Assert(val, Equals, "")
 	c.Assert(err, Equals, nil)
+}
+
+//---------
+// RPush / RPushX
+//---------
+
+// Tests if RPushX (push only if it exists), RPUSH and Delete
+func (m *MeshRedisTest) TestRPushAndRPushX(c *C) {
+	key := "someKey"
+
+	// Set pushing on a list that doesn't exist. Shouldn't work
+	count, err := session.RPushX(key, key)
+	c.Assert(err, IsNil)
+	c.Assert(count, Equals, 0)
+
+	// Confirm list count is 0
+	count, err = session.GetListCount(key)
+	c.Assert(err, IsNil)
+	c.Assert(count, Equals, 0)
+
+	// Push and create a list
+	count, err = session.RPush(key, key)
+	c.Assert(err, IsNil)
+	c.Assert(count, Equals, 1)
+
+	// Confirm list count is 1
+	count, err = session.GetListCount(key)
+	c.Assert(err, IsNil)
+	c.Assert(count, Equals, 1)
+
+	// Set pushing on a list that exists. Should work this time
+	count, err = session.RPushX(key, key)
+	c.Assert(err, IsNil)
+	c.Assert(count, Equals, 2)
+
+	// Confirm list count is 1
+	count, err = session.GetListCount(key)
+	c.Assert(err, IsNil)
+	c.Assert(count, Equals, 2)
+
+	// Delete the list
+	err = session.Delete(key)
+	c.Assert(err, IsNil)
+
+	// Confirm list count is 0
+	count, err = session.GetListCount(key)
+	c.Assert(err, IsNil)
+	c.Assert(count, Equals, 0)
+}
+
+//---------
+// Multi / Pipelining
+//---------
+
+// Tests the atomic opp + ttl + GetListCount
+func (m *MeshRedisTest) TestAtomicPushOnListWithExpiration(c *C) {
+	key := "someKey"
+
+	// Expiration is half a second
+	expiration := int64(500)
+
+	// Test getting a new MeshRedisSession
+
+	// Set list w/ expiration
+	err := session.AtomicPushOnListWithMsExpiration(key, key, expiration)
+	c.Assert(err, IsNil)
+
+	// Check if list exists and count is correct
+	count, err := session.GetListCount(key)
+	c.Assert(err, IsNil)
+	c.Assert(count, Equals, 1)
+
+	// Check TTL is greater than 0
+	ttl, err := session.PTTLForKey(key)
+	c.Assert(err, IsNil)
+	c.Assert(ttl, Not(Equals), 0)
+
+	// Sleep for time
+	timeWithMargin := float64(expiration) * 1.1
+	sleepTime := time.Duration(timeWithMargin) * time.Millisecond
+	time.Sleep(sleepTime)
+
+	// Re Run tests w/ key expired
+	// Check if list exists and count is correct
+	count, err = session.GetListCount(key)
+	c.Assert(err, IsNil)
+	c.Assert(count, Equals, 0)
+
+	// Check TTL is greater than 0
+	ttl, err = session.PTTLForKey(key)
+	c.Assert(err, IsNil)
+	c.Assert(ttl < 0, Equals, true)
 }
